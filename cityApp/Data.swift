648257cb46b14ed5ef914204879c31bdc76f7497
Vlad <vlad14196@gmail.com>
2022-01-04 18:39:00 +0300

    ver 1

---
+++ cityApp/Data.swift
@@ -0,0 +1,21 @@
+//
+//  Data.swift
+//  cityApp
+//
+//  Created by user on 04.01.2022.
+//
+
+import Foundation
+
+let cities = ["Москва,Новосибирск,Уфа,Санкт-Петербург"]
+let attractionMoscow = ["Москвовский Метрополитен,Московский Кремль,Красная площадь,Третьяковская галерея,Оружейная палата"]
+let descriptionMoscow = ["Москвовский Метрополитен": [descriptionMM],
+                         "Московский Кремль":[descriptionMK],
+                         "Красная площадь":[descriptionKP],
+                         "Третьяковская галерея":[descriptionTK],
+                         "Оружейная палата":[descriptionOP]]
+let descriptionMM = [""]
+let descriptionMK = [""]
+let descriptionKP = [""]
+let descriptionTK = [""]
+let descriptionOP = [""]
